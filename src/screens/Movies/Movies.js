2a1893af0f50c6f5615695b62eb68ddeeb680ebe
radwa-mohsen <38859200+radwa-mohsen@users.noreply.github.com>
2024-03-14 05:19:15 +0100

    Add Sort functionality

--- src/screens/Movies/Movies.js
+++ src/screens/Movies/Movies.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useCallback } from "react";
-import { MovieCard } from "../../components/common/MovieCard";
 import {
   Box,
   FormControl,
@@ -7,17 +6,21 @@ import {
   IconButton,
   InputAdornment,
   InputLabel,
+  MenuItem,
   OutlinedInput,
+  Select,
 } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { MovieCard } from "../../components/common/MovieCard";
 import axios, { API } from "../../config/axios";
 import useErrorsHandler from "../../hooks/useErrorHandler";
-import SearchIcon from "@mui/icons-material/Search";
 
 const Movies = () => {
   // States
   const [movies, setMovies] = useState([]);
   const [filteredMovies, setFilteredMovies] = useState([]);
   const [searchText, setSearchText] = useState("");
+  const [sortOption, setSortOption] = useState("");
 
   // Hooks
   const handleErrorResponse = useErrorsHandler();
@@ -38,7 +41,6 @@ const Movies = () => {
 
   const handleSearchChange = (e) => {
     const query = e.target.value;
-    !query && setFilteredMovies(movies);
     setSearchText(query);
   };
   const handleSearchClick = () => {
@@ -46,6 +48,24 @@ const Movies = () => {
       movie.title.toLowerCase().includes(searchText.toLowerCase())
     );
     setFilteredMovies(!searchText ? movies : filteredMovies);
+  };
+
+  const handleSortChange = (e) => {
+    const option = e.target.value;
+    setSortOption(option);
+
+    if (option === "rate") {
+      setFilteredMovies([...filteredMovies].sort((a, b) => b.vote_average - a.vote_average));
+    } else if (option === "date") {
+      setFilteredMovies(
+        [...filteredMovies].sort((a, b) => new Date(b.release_date) - new Date(a.release_date))
+      );
+    } else {
+      const filterBySearch = movies.filter((movie) =>
+        movie.title.toLowerCase().includes(searchText.toLowerCase())
+      );
+      setFilteredMovies(!searchText ? movies : filterBySearch);
+    }
   };
 
   // UseEffects
@@ -57,30 +77,38 @@ const Movies = () => {
     <>
       <h1> Movies List</h1>
       <Box display="flex" justifyContent="end" mb={4}>
-        <Box display="flex">
-          <FormControl variant="outlined">
-            <InputLabel htmlFor="search-field">Search</InputLabel>
-            <OutlinedInput
-              id="search-field"
-              type="text"
-              value={searchText}
-              onChange={handleSearchChange}
-              placeholder="Search by Movie Title"
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    onClick={handleSearchClick}
-                    onMouseDown={handleSearchClick}
-                    edge="end"
-                  >
-                    <SearchIcon />
-                  </IconButton>
-                </InputAdornment>
-              }
-              label="Search"
-            />
-          </FormControl>
-        </Box>
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="search-field">Search</InputLabel>
+          <OutlinedInput
+            id="search-field"
+            type="text"
+            value={searchText}
+            onChange={handleSearchChange}
+            placeholder="Search by Movie Title"
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton onClick={handleSearchClick} onMouseDown={handleSearchClick} edge="end">
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Search"
+          />
+        </FormControl>
+        <FormControl sx={{ marginLeft: "16px", minWidth: "200px" }} variant="outlined">
+          <InputLabel id="sort-label">Sort By</InputLabel>
+          <Select
+            labelId="sort-label"
+            label="Sort By"
+            variant="outlined"
+            onChange={handleSortChange}
+            value={sortOption}
+          >
+            <MenuItem value="none">None</MenuItem>
+            <MenuItem value="rate">Rating</MenuItem>
+            <MenuItem value="date">Release Date</MenuItem>
+          </Select>
+        </FormControl>
       </Box>
 
       <Grid container spacing={4}>
