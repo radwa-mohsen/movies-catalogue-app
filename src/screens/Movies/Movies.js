5548363a3e9f858736267a9e04d2b17071a82ae9
radwa-mohsen <38859200+radwa-mohsen@users.noreply.github.com>
2024-03-14 04:54:28 +0100

    Add search functionality

--- src/screens/Movies/Movies.js
+++ src/screens/Movies/Movies.js
@@ -1,12 +1,24 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import { MovieCard } from '../../components/common/MovieCard';
-import { Container, Grid } from '@mui/material';
-import axios, { API } from '../../config/axios';
-import useErrorsHandler from '../../hooks/useErrorHandler';
+import React, { useEffect, useState, useCallback } from "react";
+import { MovieCard } from "../../components/common/MovieCard";
+import {
+  Box,
+  Container,
+  FormControl,
+  Grid,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
+import axios, { API } from "../../config/axios";
+import useErrorsHandler from "../../hooks/useErrorHandler";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Movies = () => {
   // States
   const [movies, setMovies] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   // Hooks
   const handleErrorResponse = useErrorsHandler();
@@ -14,9 +26,10 @@ const Movies = () => {
   // Functions
   const getMoviesList = useCallback(() => {
     axios
-      .get(API.movies('now_playing'))
+      .get(API.movies("now_playing"))
       .then((res) => {
         setMovies(res.data.results);
+        setFilteredMovies(res.data.results);
       })
       .catch((error) => {
         // handle error
@@ -24,16 +37,55 @@ const Movies = () => {
       });
   }, [handleErrorResponse]);
 
+  const handleSearchChange = (e) => {
+    const query = e.target.value;
+    !query && setFilteredMovies(movies);
+    setSearchText(query);
+  };
+  const handleSearchClick = () => {
+    const filteredMovies = movies.filter((movie) =>
+      movie.title.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredMovies(!searchText ? movies : filteredMovies);
+  };
+
   // UseEffects
   useEffect(() => {
     getMoviesList();
   }, []);
 
-  return(
+  return (
     <Container maxWidth="xl">
       <h1> Movies List</h1>
+      <Box display="flex" justifyContent="end" mb={4}>
+        <Box display="flex">
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="search-field">Search</InputLabel>
+            <OutlinedInput
+              id="search-field"
+              type="text"
+              value={searchText}
+              onChange={handleSearchChange}
+              placeholder="Search by Movie Title"
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={handleSearchClick}
+                    onMouseDown={handleSearchClick}
+                    edge="end"
+                  >
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Search"
+            />
+          </FormControl>
+        </Box>
+      </Box>
+
       <Grid container spacing={4}>
-        {movies.map((movie) => (
+        {filteredMovies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
       </Grid>
