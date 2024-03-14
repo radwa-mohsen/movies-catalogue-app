3358e49c5a263920d70e50fa500cd72f3ac827f2
radwa-mohsen <38859200+radwa-mohsen@users.noreply.github.com>
2024-03-14 03:37:26 +0100

    Fetch and Render Movie Data

--- src/screens/Movies/Movies.js
+++ src/screens/Movies/Movies.js
@@ -1,14 +1,38 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import { MovieCard } from '../../components/common/MoviewCard'
+import React, { useEffect, useState } from 'react';
+import { MovieCard } from '../../components/common/MoviewCard';
+import { Box } from '@mui/material';
+import axios, { API } from '../../config/axios';
 
 const Movies = () => {
-  return (
-    <Box>
-      <h1> Movies List</h1>
-      <MovieCard/>
-    </Box>
-    )
-}
-
-export {Movies}
+    // States
+    const [movies, setMovies] = useState([]);
+  
+    // Functions
+    const getMoviesList = () => {
+      axios
+        .get(API.movies('now_playing'))
+        .then((res) => {
+          setMovies(res.data.results);
+        })
+        .catch((error) => {
+          // handle error
+          console.log(error);
+        });
+    };
+  
+    // UseEffects
+    useEffect(() => {
+      getMoviesList();
+    }, []);
+  
+    return (
+      <Box>
+        <h1> Movies List</h1>
+        {movies?.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </Box>
+    );
+  };
+  
+  export { Movies };
